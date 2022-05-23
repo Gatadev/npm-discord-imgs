96683ffa9a7affe6fcf63ca69690c77e3a8a9082
gatadev <kairateadoro@gmail.com>
2022-05-23 14:55:32 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# discordimgs
+
+Este es un paquete por el cual podrás obtener imágenes de cualquier tipo aleatoriamente, no es la gran cosa, pero al menos tiene contenido multimedia...
+
+## Instalacion
+Instala el paquete usando la consola o buscándolo en la selección de paquetes: `discord-imgs`.
+```
+$ npm i discordimgs
+```
+## Uso:
+```js
+const dimgs = require('discord-imgs');
+```
+
+## Metodos disponibles:
+
+```js
+//Memes
+dimgs.randomMemeAll()
+dimgs.randomMemeVideo()
+dimgs.randomMemeImagen()
+
+/* ==== Proximamente mas ==== */
+```
+
+## Ejemplos:
+
+### Memes:
+
+ - Memes de cualquier formato `mp4` `png` `jpg` `gif` etc..
+
+```js
+//Definir el paquete:
+const dimgs = require('discord-imgs');
+
+console.log(dimgs.randomMemeAll())
+/*Devolvera un link de discord que sera de un meme,
+ya sea imagen o video.*/
+```
+
+ - Memes de solo imágenes.
+```js
+//Definir el paquete:
+const dimgs = require('discord-imgs');
+
+console.log(dimgs.randomMemeImagen())
+/*Devolvera un link de discord random
+que solo seran de imagenes*/
+```
+
+ - Memes de solo vídeos.
+ 
+```js
+//Definir el paquete:
+const dimgs = require('discord-imgs');
+
+console.log(dimgs.randomMemeVideo())
+/*Devolvera un link de discord random
+que solo seran de videos*/
+```
---
+++ index.js
@@ -0,0 +1,93 @@
+const memesVideos = [
+   "https://cdn.discordapp.com/attachments/738683291829403658/756037833336291369/67286b69547243759ce2271d6bae48c0.mp4",
+   "https://cdn.discordapp.com/attachments/767476484545576971/767496081814323211/video0-8-2.mp4",
+   "https://cdn.discordapp.com/attachments/767476484545576971/767496515907747840/VID-20200803-WA0006.mp4",
+   "https://cdn.discordapp.com/attachments/767476484545576971/767496562657591297/118632129_2905454999558507_2123656878953473089_n_2.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/767024366868234284/reject_humanity_return_to_monkey.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/765960010013999194/https___t.co_YfK9RDo51Z.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/765623336532312124/pi_pip_pi_pi_pi_pu.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/764677397970681876/UwyUJwbZXshODCYm-1.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/764554285227835402/CSS.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/760873810290540544/memedealar_1601268784444698.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/760720780576948244/El_Pepe.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/759273184931086405/118792210_1476793565854634_5689423070103626664_n.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/759273168408936488/119985591_802112317269709_5086461620556041486_n.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/759273057457668146/119663671_187245239442149_6649161716638894889_n.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/759209780366934026/clash_of_clan_lightnig_spell_1.mp4",
+   "https://cdn.discordapp.com/attachments/722963618475278398/758906220202950676/120165886_267515217691613_2916955935795418570_n.mp4",
+   "https://cdn.discordapp.com/attachments/752941675868848128/767303943210008576/YesItsme-PublicacionesFacebook21.mp4",
+   "https://cdn.discordapp.com/attachments/752941675868848128/767290553988546600/israel_israntt_Fotos_y_videos_de_Instagram.mp4",
+   "https://cdn.discordapp.com/attachments/752941675868848128/767290553062129684/pol.mp4",
+   "https://cdn.discordapp.com/attachments/752941675868848128/767273460165640212/When_two_rocks_look_identical360P_1-2-1.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/767517522714820608/callao_lacra_ando_escuchando_pvz.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/767439120627662868/El_mejor_boludo_de_la_moto360P_1.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/765984355083616256/rusos.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/765803211410898954/se_va_del_chat.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/765658469138694144/fafgad.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/765216344836407296/y2mate.com_-_Escapando_de_la_policia._version_Maradona_1848NrUzKxE_360p.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/764883034285408346/has-visto-muchos-memes-descansa-un-poco.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/767520725380104212/video0-15-1.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/767522151284473856/Facebook_undefined360p_2-1.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/767852353868529674/Facebook_368720904509788360p_1-1.mp4",
+   "https://cdn.discordapp.com/attachments/635581932603834408/767522267823210546/video0-19.mp4"
+  ];
+  
+const memesImagen = [
+  "https://media.discordapp.net/attachments/767476484545576971/767487266779168837/Aqui_entre_panas.png",
+  "https://media.discordapp.net/attachments/722963618475278398/766945581695238144/c4f.png?width=616&height=499",
+  "https://media.discordapp.net/attachments/722963618475278398/766586615186456626/image0-7.png?width=491&height=499",
+  "https://media.discordapp.net/attachments/722963618475278398/765908269767852032/image0-111.png?width=456&height=499",
+  "https://media.discordapp.net/attachments/722963618475278398/765638745721929728/image0-50.png",
+  "https://media.discordapp.net/attachments/722963618475278398/765619569355390996/PicsArt_10-01-06.17.10.jpg",
+  "https://media.discordapp.net/attachments/722963618475278398/763648898081751050/15847080024382819636618982751331-2.jpg?width=514&height=499",
+  "https://media.discordapp.net/attachments/722963618475278398/762329502255677460/FB_IMG_1601307005211.png",
+  "https://media.discordapp.net/attachments/722963618475278398/761634017401110528/6ltxh0tjuuc51.png?width=422&height=498",
+  "https://media.discordapp.net/attachments/722963618475278398/758866494800527369/dias_sin_reirme.png",
+  "https://media.discordapp.net/attachments/752941675868848128/767426455192862730/463e2d8d13008f4d844cfab1ca82366e.jpg",
+  "https://media.discordapp.net/attachments/627957807848882177/767517289338896394/image0-1.png",
+  "https://media.discordapp.net/attachments/635581932603834408/767086995985793064/image0-4.png?width=433&height=432",
+  "https://images-ext-2.discordapp.net/external/eInvCDnds28z69vFHHo6RzJ9etDhasKnomc6zPBNZm4/%3Fwidth%3D478%26height%3D475/https/media.discordapp.net/attachments/635581932603834408/767517548962250792/Screenshot_2020-10-17-20-07-202.png?width=435&height=432",
+  "https://images-ext-1.discordapp.net/external/rRggVq9qXT5XPCleFahzFXvKxhp8hoFj8TyYk75wfiI/https/media.discordapp.net/attachments/635581932603834408/767087257315835924/5f19b670e87f1.png?width=432&height=432",
+  "https://images-ext-2.discordapp.net/external/gS3ojloUAkbU3Mz6LRFXfE8sDboRySE54dKtEC2_JV4/https/media.discordapp.net/attachments/635581932603834408/766429961363259392/121463519_642054069727668_1665536517160574351_n.png",
+  "https://images-ext-2.discordapp.net/external/A4lkxuRTOBE0OtunhYscl2VZEy4wZnUUygp-j3XUcGI/https/media.discordapp.net/attachments/635581932603834408/766421314658631680/Screenshot_20200824_175522.png",
+  "https://media.discordapp.net/attachments/635581932603834408/766383489683030036/IMG_20201007_213819_173.png",
+  "https://images-ext-1.discordapp.net/external/pTMtMPvxCNbEPm99r_a5gsykkb5QPbqb-1TdaMW77IU/https/media.discordapp.net/attachments/635581932603834408/766304729705480222/IMG-20200914-WA0008.png?width=599&height=432",
+  "https://images-ext-1.discordapp.net/external/Wj1L5FB7TCm_2cOKtveoMV2znMcJYBdu9vna_LadZ2Y/https/media.discordapp.net/attachments/635581932603834408/765989712787669022/Screenshot_20200919_131715.png?width=436&height=432",
+  "https://media.discordapp.net/attachments/635581932603834408/765804758799613970/images_17-1.png",
+  "https://media.discordapp.net/attachments/635581932603834408/765709073827495936/5e6edc1ada3ca.png",
+   "https://media.discordapp.net/attachments/635581932603834408/765577148538159134/image0-2.png?width=447&height=432",
+   "https://media.discordapp.net/attachments/635581932603834408/765341828479320133/image0-2.png?width=434&height=432",
+   "https://media.discordapp.net/attachments/635581932603834408/765341553623171132/IMG-20200908-WA0093.png?width=667&height=432",
+   "https://media.discordapp.net/attachments/635581932603834408/765331083985420288/Screenshot_20200820_184956.png",
+   "https://media.discordapp.net/attachments/635581932603834408/765317358046150666/c6c22f5102ab28ce402f2656f64960bf.png",
+   "https://media.discordapp.net/attachments/635581932603834408/765239156967342147/Screenshot_20201008_082006.png?width=359&height=432",
+   "https://images-ext-2.discordapp.net/external/hoQFOeS4cn8qNbvPokAXT_UodVBxIDGu2oaKzo5BCD4/https/media.discordapp.net/attachments/635581932603834408/765240256692879360/image0-24.png?width=323&height=432",
+   "https://media.discordapp.net/attachments/635581932603834408/765219297516978216/IMG_20201011_132950.jpg?width=431&height=432",
+   "https://media.discordapp.net/attachments/635581932603834408/765218991895216158/SPOILER_SPOILER_2020-06-14_6.png",
+   "https://media.discordapp.net/attachments/635581932603834408/765218586540507176/FB_IMG_1601849579329.jpg?width=376&height=432",
+   "https://media.discordapp.net/attachments/635581932603834408/765218274828353586/PicsArt_10-06-09.png"
+]
+
+  /**
+   * @returns {string}
+   */
+
+  function randomMemeVideo() {
+    return memesVideos[Math.floor(Math.random() * memesVideos.length)];
+  }
+  
+  function randomMemeImagen() {
+    return memesImagen[Math.floor(Math.random() * memesImagen.length)];
+  }
+
+  function randomMemeAll() {
+    let array = [memesVideos, memesImagen]; array = array[Math.floor(Math.random() * array.length)];
+    return array[Math.floor(Math.random() * array.length)];
+
+  }
+
+  module.exports = {
+    randomMemeVideo,
+    randomMemeImagen,
+    randomMemeAll
+  };
